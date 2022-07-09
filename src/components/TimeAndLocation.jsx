2e018f079776219b4b41b6a80fc4cab25b602f20
Jonesdy99 <jonesdylan1165@gmail.com>
2022-07-09 15:46:23 -0400

    implement more nav-bar changes

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className='flex items-center justify-center my-6'>
+        <p className='text-white text-xl font-extralight'>
+        Tuesday, 31 May 2022 | Local Time 12:46PM
+        </p>
+      </div>
+      <div className='flex items-center justify-center my-3'>
+        <p className='text-white text-3xl font-medium'>
+          Berlin
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
