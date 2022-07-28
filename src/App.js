b9cfc63e1ccc9608d02cf123d72815043ef7809c
Jonesdy99 <jonesdylan1165@gmail.com>
2022-07-28 14:02:21 -0400

    implement tempanddetails function into app

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import ForeCast from './components/ForeCast';
 import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
-  const fetchWeather = async() => {
-    const data = await getFormattedWeatherData({q: 'london'})
-    console.log(data)
-  }
+  const [query, setQuery] = useState({q: 'berlin'})
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
 
-  fetchWeather()
+  useEffect(() => {
+    const fetchWeather = async() => {
+      await getFormattedWeatherData({...query, units}).then(data => {
+        setWeather(data)
+      })
+    }
+    fetchWeather()
+  }, [query, units])
 
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
       <TopButtons/>
       <Inputs/>
 
-      <TimeAndLocation/>
-      <TemperatureAndDetails/>
-
-      <ForeCast title='hourly forecast'/>
-      <ForeCast title='daily forecast'/>
+      {weather && (
+        <div>
+        <TimeAndLocation weather={weather} />
+        <TemperatureAndDetails weather={weather}/>
+  
+        <ForeCast title='hourly forecast'/>
+        <ForeCast title='daily forecast'/>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -8,16 +8,17 @@ import {
   UilSun,
   UilSunset,
 } from '@iconscout/react-unicons'
+import { iconUrlFromCode } from '../services/weatherService'
 
-function TemperatureAndDetails() {
+function TemperatureAndDetails({weather: { details, icon, temp, temp_min, temp_max, sunrise, sunset, speed, humidity, feels_like, timezone}}) {
   return (
     <div>
       <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
-        <p>Cloudy</p>
+        <p>{details}</p>
       </div>
       <div className='flex flex-row items-center justify-between text-white py-3'>
-        <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" className='w-20'/>
-        <p className='text-5xl'>34°</p>
+        <img src={iconUrlFromCode(icon)} alt="" className='w-20'/>
+        <p className='text-5xl'>{`${temp.toFixed()}°`}</p>
         <div className='flex flex-col space-y-2'>
           <div className='flex font-light text-sm items-center justify-center'>
             <UilTemperature size={18} className='mr-1'/>
